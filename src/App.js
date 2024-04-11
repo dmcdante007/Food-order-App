f471cb9483cc02b8449ce629b2dee695eb04383f
dmcdante007 <meshramswaraj98>
2024-04-11 15:08:11 +0530

    'Meal-summary-box'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React,{Fragment} from "react";
 import Header from "./components/Layout/Header";
+import MealSummary from "./components/Layout/MealSummary";
 
 
 function App() {
   return (
     <Fragment>
     <Header/>
+    <MealSummary/>
     </Fragment>
   );
 }

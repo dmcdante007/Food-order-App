372f7bb0f70a220b9d03f8b6735fe2f6b5a073e8
dmcdante007 <meshramswaraj98>
2024-04-12 17:13:57 +0530

    'Update-cart-button'

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,25 @@
 import React from "react";
-import classes from './MealItemForm.module.css'
+import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
 
-const MealItemForm = (props)=>{
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
 
-    return <div>
-    Amount{props.countis}<br/>
-    <button onClick={props.onClickingAdd}>+Add</button>
-</div>
-}
+      <button onClick={props.onClickingAdd}>+Add</button>
+    </form>
+  );
+};
 
 export default MealItemForm;
